56f7702a739b96d688e5f3ee7c1dbd76587330db
Geoff Bremner <geoffbremner@Geoffs-MacBook-Pro.local>
2021-08-09 09:57:34 -0400

    visual knobs functional

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -39,12 +39,23 @@ private:
     SimpleEQAudioProcessor& audioProcessor;
     
     CustomRotarySlider peakFreqSlider,
-    peakGainSlider,
-    peakQualitySlider,
-    lowCutFreqSlider,
-    highCutFreqSlider,
-    lowCutSlopeSlider,
-    highCutSlopeSlider;
+        peakGainSlider,
+        peakQualitySlider,
+        lowCutFreqSlider,
+        highCutFreqSlider,
+        lowCutSlopeSlider,
+        highCutSlopeSlider;
+    
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachment = APVTS::SliderAttachment;
+    
+    Attachment peakFreqSliderAttachment,
+        peakGainSliderAttachment,
+        peakQualitySliderAttachment,
+        lowCutFreqSliderAttachment,
+        highCutFreqSliderAttachment,
+        lowCutSlopeSliderAttachment,
+        highCutSlopeSliderAttachment;
     
     std::vector<juce::Component*> getComps();
 
