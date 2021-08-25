7cc99966a4a2161761b6e894ebe329299ead81e6
Geoff Bremner <geoffbremner@Geoffs-MacBook-Pro.local>
2021-08-25 16:31:59 -0400

    roughed out RTA grid lines

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -67,6 +67,7 @@ juce::Timer
     void timerCallback() override;
     
     void paint(juce::Graphics& g) override;
+    void resized() override;
 private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged { false };
@@ -74,12 +75,16 @@ private:
     MonoChain monoChain;
     
     void updateChain();
+    
+    juce::Image background;
+    juce::Rectangle<int> getRenderArea();
+    juce::Rectangle<int> getAnalysisArea();
 };
 
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
